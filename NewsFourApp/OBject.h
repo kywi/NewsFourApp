a6c76ceb6f0b84ddd89c0821e1225c1bd6dea153
kywi <775849209@qq.com>
2016-02-15 15:29:00 +0800

    添加了一个类

---
+++ NewsFourApp/OBject.h
@@ -0,0 +1,13 @@
+//
+//  OBject.h
+//  NewsFourApp
+//
+//  Created by 倪斌 on 16/2/15.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OBject : NSObject
+
+@end
---
+++ NewsFourApp/OBject.m
@@ -0,0 +1,13 @@
+//
+//  OBject.m
+//  NewsFourApp
+//
+//  Created by 倪斌 on 16/2/15.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+#import "OBject.h"
+
+@implementation OBject
+
+@end
